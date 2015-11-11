8473c8d8a15060aab501c4a9334178f774c6072a
Tazro Inutano Ohta <inutano@gmail.com>
2015-11-11 18:02:25 +0900

    fixed indentation

--- lib/quanto/tables.rb
+++ lib/quanto/tables.rb
@@ -18,25 +18,26 @@ module Quanto
 				sra_ftp_base = "ftp.ncbi.nlm.nih.gov/sra/reports/Metadata"
 				# filename
 				ym = Time.now.strftime("%Y%m")
-		    tarball = "NCBI_SRA_Metadata_Full_#{ym}01.tar.gz"
+				tarball = "NCBI_SRA_Metadata_Full_#{ym}01.tar.gz"
 				dest_file = File.join(dest_dir, tarball)
 				# download via ftp
-		    sh "lftp -c \"open #{sra_ftp_base} && pget -n 8 -o #{dest_dir} #{tarball}\""
-		    sh "tar zxf #{dest_file}"
+				sh "lftp -c \"open #{sra_ftp_base} && pget -n 8 -o #{dest_dir} #{tarball}\""
+				sh "tar zxf #{dest_file}"
+				fix_sra_metadata_directory(dest_file.sub(/.tar.gz/,""))
 				rm_f dest_file
 			end
 
 			def fix_sra_metadata_directory(metadata_parent_dir)
 				cd metadata_parent_dir
-		    acc_dirs = Dir.entries(metadata_parent_dir).select{|f| f =~ /^.RA\d{6,7}$/ }
-		    acc_dirs.group_by{|id| id.sub(/...$/,"") }.each_pair do |pid, ids|
-		      moveto = File.join(sra_metadata, pid)
-		      mkdir moveto
-		      mv ids, moveto
-		    end
+				acc_dirs = Dir.entries(metadata_parent_dir).select{|f| f =~ /^.RA\d{6,7}$/ }
+				acc_dirs.group_by{|id| id.sub(/...$/,"") }.each_pair do |pid, ids|
+					moveto = File.join(sra_metadata, pid)
+					mkdir moveto
+					mv ids, moveto
+				end
 			end
 
-			def create_list_finished
+			def create_list_fastqc_finished
 			end
 
 			def create_list_layout
