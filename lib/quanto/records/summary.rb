f8e9f56e707692f75f49b69e4759f77367df8361
Tazro Inutano Ohta <inutano@gmail.com>
2016-03-22 19:49:06 +0900

    refactored create_summary

--- lib/quanto/records/summary.rb
+++ lib/quanto/records/summary.rb
@@ -9,7 +9,8 @@ module Quanto
     class Summary
       class << self
         def set_number_of_parallels(nop)
-          @@num_of_parallels = nop
+          # class variable: number of parallels
+          @@nop = nop
         end
 
         def summarize(list_fastqc_finished, outdir)
@@ -25,22 +26,16 @@ module Quanto
         end
 
         def create_summary(path_list, outdir)
-          puts "Set number of parallel: #{@@num_of_parallels} threads."
-          p_mes = "Creating FastQC summary files"
-          Parallel.each(path_list, :in_threads => @@num_of_parallels, :progress => p_mes) do |path|
-            # extract file id
-            fileid = path2fileid(path)
-
-            # generate summary of fastqc data
+          p_mes = "Creating summary files"
+          Parallel.map(path_list, :in_threads => @@nop, :progress => p_mes) do |path|
+            fileid  = path2fileid(path)
+            dir     = summary_file_dir(outdir, fileid)
+            FileUtils.mkdir_p(dir)
             summary = summarize_fastqc(path)
-
-            # save summary files
-            ["json", "jsonld", "ttl", "tsv"].each do |format|
-              output_path = summary_file(outdir, fileid, format)
-              if !File.exist?(output_path)
-                Bio::FastQC::IO.new(summary, id: fileid).write(output_path, format)
-              end
-            end
+            write_summary_file(summary, fileid, dir, "json")
+            write_summary_file(summary, fileid, dir, "tsv")
+            write_summary_file(summary, fileid, dir, "ttl")
+            nil
           end
         end
 
@@ -48,16 +43,18 @@ module Quanto
           path.split("/").last
         end
 
-        def summary_file(outdir, fileid, ext)
+        def summary_file_dir(outdir, fileid)
           # create directory
           id = fileid.sub(/_.+$/,"")
           center = id.slice(0,3)
           prefix = id.slice(0,4)
           index  = id.sub(/...$/,"")
-          dir    = File.join(outdir, center, prefix, index, id)
-          FileUtils.mkdir_p(dir)
+          File.join(outdir, center, prefix, index, id)
+        end
 
+        def summary_file_path(outdir, fileid, ext)
           # returns path like /path/to/out_dir/DRR/DRR0/DRR000/DRR000001/DRR000001_fastqc.json
+          dir = summary_file_dir(outdir, fileid)
           summary_file_name = fileid.sub(".zip",".#{ext}")
           File.join(dir, summary_file_name)
         end
@@ -67,13 +64,17 @@ module Quanto
           Bio::FastQC::Parser.new(data).summary
         end
 
+        def write_summary_file(summary, fileid, dir, format)
+          Bio::FastQC::IO.new(summary, id: fileid).write(File.join(dir, fileid+"."+format), format)
+        end
+
         def create_list(path_list, outdir)
-          relpath = outdir.sub(/^.+summary\//,"")
-          list = path_list.map do |path|
+          p_mes = "Creating file list"
+          list = Parallel.map(path_list, :in_threads => @@nop, :progress => p_mes) do |path|
             fileid = path2fileid(path)
-            summary_file(relpath, fileid, "json")
+            summary_file_path(outdir, fileid, "json").sub(/^.+summary\//,"")
           end
-          fname_out = File.join(outdir, "summary_list")
+          list_file_path = File.join(outdir, "summary_list")
           backup = fname_out + "." + Time.now.strftime("%Y%m%d")
           FileUtils.mv(fname_out, backup) if File.exist?(fname_out)
           open(fname_out, "w"){|file| file.puts(list) }
