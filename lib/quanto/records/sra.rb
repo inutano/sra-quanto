73c5c5d030c4cbb3eaff64fc99071e2ccd60ec45
Tazro Inutano Ohta <inutano@gmail.com>
2016-10-19 15:21:54 +0900

    fixed shell execution

--- lib/quanto/records/sra.rb
+++ lib/quanto/records/sra.rb
@@ -52,12 +52,12 @@ module Quanto
         end
 
         def fix_sra_metadata_directory(metadata_parent_dir)
-          cd metadata_parent_dir
+          RakeFileUtils.sh "cd #{metadata_parent_dir}"
           pdir = get_accession_directories(metadata_parent_dir)
           pdir.group_by{|id| id.sub(/...$/,"") }.each_pair do |pid, ids|
             moveto = File.join(metadata_parent_dir, pid)
-            mkdir moveto
-            mv ids, moveto
+            RakeFileUtils.sh "mkdir #{moveto}"
+            RakeFileUtils.sh "mv #{ids}, #{moveto}"
           end
         end
 
