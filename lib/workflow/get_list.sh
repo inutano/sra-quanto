35cdcba5c92898c238b124c97784bcc82f8b66d6
tazro inutano ohta <inutano@gmail.com>
2018-10-26 13:13:41 +0900

    resoleve symlink and add temp dir

--- lib/workflow/get_list.sh
+++ lib/workflow/get_list.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 set -eu
 
+# Path to directory of _fastqc.zip files: Exit if not passed
+TARGET_DIR_PATH="${1}"
+TARGET_DIR="$(cd $(dirname "${TARGET_DIR_PATH}") && pwd -P)/$(basename "${TARGET_DIR_PATH}")"
+
 # Get directory path
 WORKDIR=$(cd $(dirname "${0}") && pwd -P)
+TMPDIR="${WORKDIR}/$(date +%Y%m%d-%H%M)"
+mkdir -p "${TMPDIR}"
 
-# Path to directory of _fastqc.zip files
-TARGET_DIR="${1}"
+# Squash
+DONE_LIST="${TMPDIR}/done.txt"
+LIVE_LIST="${TMPDIR}/live.txt"
+UNDONE_LIST="${TMPDIR}/undone.txt"
 
 # Get DONE list
-DONE_LIST="${WORKDIR}/done.txt"
 find "${TARGET_DIR}" -name '*_fastqc.zip' |\
   xargs -I{} basename {} |\
   sed -e 's:_fastqc.zip$::g' |\
   sort > "${DONE_LIST}"
 
 # Get LIVE list
-LIVE_LIST="${WORKDIR}/live.txt"
 curl "ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata/SRA_Run_Members.tab" |\
   awk -F'\t' '$8 == "live" { print $1 }' |\
   sort > "${LIVE_LIST}"
 
 # Get UNDONE list
-UNDONE_LIST="${WORKDIR}/undone.txt"
 diff "${DONE_LIST}" "${LIVE_LIST}" | awk '/^>/' > "${UNDONE_LIST}"
