6fa2928a94c62e0f14d207379dad43a69d3a5cfe
tazro inutano ohta <inutano@gmail.com>
2018-10-26 13:02:21 +0900

    simple list creation script

---
+++ lib/workflow/get_list.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Get current directory of the script
+WORKDIR=$(basename "${0}")
+
+# Path to directory of _fastqc.zip files
+TARGET_DIR="${1}"
+
+# Get DONE list
+DONE_LIST="${WORKDIR}/done.txt"
+find "${TARGET_DIR}" -name '*_fastqc.zip' |\
+  xargs -I{} basename {} |\
+  sed -e 's:_fastqc.zip$::g' |\
+  sort > "${DONE_LIST}"
+
+# Get LIVE list
+LIVE_LIST="${WORKDIR}/live.txt"
+curl "ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata/SRA_Run_Members.tab" |\
+  awk -F'\t' '$8 == "live" { print $1 }' |\
+  sort > "${LIVE_LIST}"
+
+# Get UNDONE list
+UNDONE_LIST="${WORKDIR}/undone.txt"
+diff "${DONE_LIST}" "${LIVE_LIST}" | awk '/^>/' > "${UNDONE_LIST}"
